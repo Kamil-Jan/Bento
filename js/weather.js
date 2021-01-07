0a715b32570d04d4a68781f8326fcdc86c6cb2f6
Kamil-Jan <kamilyakubov80@gmail.com>
2021-01-07 11:23:51 +0300

    changed greeting, weather api and sections

--- js/weather.js
+++ js/weather.js
@@ -13,7 +13,7 @@ var tempUnit = 'C';
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
-const key = 'aa5b0a76dfbf87441928fb3cc32d3d25';
+const key = 'f40ee1b8019518e7db8d4ad2e33784fa';
 
 // Set Position function
 setPosition();
@@ -21,8 +21,8 @@ setPosition();
 function setPosition(position) {
   // Here you can change your position
   // You can use https://www.latlong.net/ to get it! (I use San Francisco as an example)
-  let latitude = 37.774929;
-  let longitude = -122.419418;
+  let latitude = 55.7558;
+  let longitude = 37.6173;
 
   getWeather(latitude, longitude);
 }
